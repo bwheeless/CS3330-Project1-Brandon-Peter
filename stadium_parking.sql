16a9445cda450fa6766e157487770ca5e241fef8
phongsun <60372665+phongsun@users.noreply.github.com>
2022-02-10 11:27:14 -0600

    created my tables

--- stadium_parking.sql
+++ stadium_parking.sql
@@ -1,5 +1,26 @@
 CREATE DATABASE stadium;
 
--- CREATE TABLE stadium(
---     id SERIAL PRIMARY KEY
--- );
+CREATE TABLE stadium(
+    id SERIAL PRIMARY KEY,
+    lot_id SERIAL,
+    event_id SERIAL
+);
+
+
+CREATE TABLE parking_space(
+     spot_number SERIAL PRIMARY KEY,
+     is_available BOOL,
+     is_handicap BOOL,
+     license_plate VARCHAR(7)
+);
+
+CREATE TABLE parking_lot(
+    lot_id SERIAL PRIMARY KEY,
+    entry_id SERIAL,
+    spot_number INT
+);
+
+CREATE TABLE event(
+    event_id SERIAL PRIMARY KEY,
+    event_type VARCHAR
+);
