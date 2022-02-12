fecb71cc5c90959ab0a88e009daadf6d9dba3219
bwheeless <55924876+bwheeless@users.noreply.github.com>
2022-02-11 22:29:31 -0600

    Update stadium_parking.sql

--- stadium_parking.sql
+++ stadium_parking.sql
@@ -1,12 +1,14 @@
 CREATE DATABASE stadium_parking;
 
 CREATE TABLE stadium(
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    address VARCHAR(30) NOT NULL,
+    name VARCHAR(15) NOT NULL
 );
 
 
 CREATE TABLE parking_lot(
-    id INTEGER NOT NULL PRIMARY KEY,
+    name CHAR NOT NULL PRIMARY KEY,
     entry_id SERIAL,
     stadium_id SERIAL REFERENCES stadium(id)
 );
@@ -19,24 +21,26 @@ CREATE TABLE event(
 
 CREATE TABLE vehicle(
     license_plate VARCHAR(8) NOT NULL PRIMARY KEY,
-    type VARCHAR(5) NOT NULL
+    type VARCHAR(5) NOT NULL,
+    is_handicap BOOL NOT NULL
 );
 
 CREATE TABLE parking_space(
      spot_number INTEGER NOT NULL PRIMARY KEY,
      is_available BOOL,
-     is_handicap BOOL,
+     is_handicap BOOL NOT NULL,
      vehicle_id VARCHAR REFERENCES vehicle(license_plate),
-     lot_id INTEGER REFERENCES parking_lot(id)
+     lot_id CHAR REFERENCES parking_lot(name)
 );
 
 CREATE TABLE entrypoint(
-    id INTEGER NOT NULL PRIMARY KEY
+    id CHAR NOT NULL PRIMARY KEY
+
 );
 
 CREATE TABLE lot_entrypoints(
-  parking_lot_id INTEGER REFERENCES parking_lot(id),
-  entrypoint_id INTEGER REFERENCES entrypoint(id),
+  parking_lot_id CHAR REFERENCES parking_lot(name),
+  entrypoint_id CHAR REFERENCES entrypoint(id),
   PRIMARY KEY (parking_lot_id, entrypoint_id)
 );
 
@@ -44,13 +48,13 @@ CREATE TABLE lot_entrypoints(
 CREATE TABLE ticket(
     id INTEGER NOT NULL PRIMARY KEY,
     vehicle_id VARCHAR NOT NULL REFERENCES vehicle(license_plate),
-    entrypoint_id SERIAL REFERENCES entrypoint(id)
+    entrypoint_id CHAR REFERENCES entrypoint(id)
 );
 
 CREATE TABLE employee(
     first_name VARCHAR(10) NOT NULL,
     last_name VARCHAR(10) NOT NULL,
     PRIMARY KEY (first_name, last_name),
-    entrypoint_id SERIAL REFERENCES entrypoint(id)
+    entrypoint_id CHAR REFERENCES entrypoint(id)
 );
 
