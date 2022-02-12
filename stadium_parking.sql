561173741fd3f85673763f800e71b0ebcd621f19
bwheeless <55924876+bwheeless@users.noreply.github.com>
2022-02-11 18:48:10 -0600

    Update stadium_parking.sql

--- stadium_parking.sql
+++ stadium_parking.sql
@@ -24,3 +24,25 @@ CREATE TABLE event(
     event_id SERIAL PRIMARY KEY,
     event_type VARCHAR
 );
+
+CREATE TABLE vehicle(
+    license_plate VARCHAR(8) NOT NULL PRIMARY KEY,
+    type VARCHAR(5) NOT NULL
+);
+
+CREATE TABLE ticket(
+    id SERIAL PRIMARY KEY,
+    vehicle_id VARCHAR NOT NULL REFERENCES vehicle(license_plate)
+);
+
+CREATE TABLE employee(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(10) NOT NULL,
+    last_name VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE entrypoint(
+    id SERIAL PRIMARY KEY,
+    ticket_num SERIAL REFERENCES ticket(id),
+    employee_num SERIAL REFERENCES employee(id)
+);
