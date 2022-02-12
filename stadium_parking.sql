83fb93001bb8e072817f2e21f5e37632b3eecb7d
bwheeless <55924876+bwheeless@users.noreply.github.com>
2022-02-11 22:20:43 -0600

    Updated tables

--- stadium_parking.sql
+++ stadium_parking.sql
@@ -1,38 +1,40 @@
-CREATE DATABASE stadium;
+CREATE DATABASE stadium_parking;
 
 CREATE TABLE stadium(
-    id SERIAL PRIMARY KEY,
-    lot_id SERIAL,
-    event_id SERIAL
+    id SERIAL PRIMARY KEY
 );
 
-
-CREATE TABLE parking_space(
-     spot_number SERIAL PRIMARY KEY,
-     is_available BOOL,
-     is_handicap BOOL,
-     license_plate VARCHAR(7)
-);
 
 CREATE TABLE parking_lot(
-    lot_id SERIAL PRIMARY KEY,
+    id INTEGER NOT NULL PRIMARY KEY,
     entry_id SERIAL,
-    spot_number INT
+    stadium_id SERIAL REFERENCES stadium(id)
 );
 
 CREATE TABLE event(
     event_id SERIAL PRIMARY KEY,
-    event_type VARCHAR
-);
-
-CREATE TABLE entrypoint(
-    id SERIAL PRIMARY KEY
+    event_type VARCHAR,
+    stadium_id SERIAL REFERENCES stadium(id)
 );
 
 CREATE TABLE vehicle(
     license_plate VARCHAR(8) NOT NULL PRIMARY KEY,
     type VARCHAR(5) NOT NULL
 );
+
+CREATE TABLE parking_space(
+     spot_number INTEGER NOT NULL PRIMARY KEY,
+     is_available BOOL,
+     is_handicap BOOL,
+     vehicle_id VARCHAR REFERENCES vehicle(license_plate),
+     lot_id INTEGER REFERENCES parking_lot(id)
+);
+
+CREATE TABLE entrypoint(
+    id SERIAL PRIMARY KEY,
+    parking_lot_id SERIAL REFERENCES parking_lot(id)
+);
+
 
 CREATE TABLE ticket(
     id SERIAL PRIMARY KEY,
