399db6b604f596528235d8c2bb3a5c9053343ec9
phongsun <60372665+phongsun@users.noreply.github.com>
2022-02-14 14:49:15 -0600

    Update parking_select_statements.sql

--- parking_select_statements.sql
+++ parking_select_statements.sql
@@ -1,4 +1,8 @@
+-- How many total parking spaces do I have?
 select * from public.parking_space;
+-- How many parking lots are there at Cowpokes Stadium?
 select stadium_id from public.parking_lot where stadium_id = 1; 
+-- How many parking spaces are there at Cowpokes Stadium
 select lot_id from public.parking_space where lot_id = '1' or lot_id = '2' or lot_id = '3';
+-- What parking spaces are currently available for use
 select * from public.parking_space where (lot_id = '1' or lot_id = '2' or lot_id = '3') and (is_available = true);
